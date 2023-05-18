67f46eb5055d06a5048c2c48faf3ac68de5e9e34
einstbro <133631045+einstbro@users.noreply.github.com>
2023-05-18 11:12:49 +0530

    Delete Django Web Framework directory

--- Django Web Framework/Models using Foreign Keys/workplace/myproject/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-# Import the models from models.py
-from .models import Drinks
-from .models import DrinksCategory
-
-# Register your models here.
-admin.site.register(Drinks)
-admin.site.register(DrinksCategory)
